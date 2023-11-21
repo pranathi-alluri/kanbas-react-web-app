ba4fae0d942ee0f2e4f591138427b0265717c3e6
Pranathi13 <alluri.p@northeastern.edu>
2023-11-21 00:31:40 -0500

    minor changes to connect with render

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 
 function Kanbas() {
     const [courses, setCourses] = useState([])
-    const URL = "http://localhost:4000/api/courses";
+    const URL = "https://kanbas-node-server-app-xtj8.onrender.com/api/courses";
     const findAllCourses = async () => {
         const response = await axios.get(URL);
         setCourses(response.data);
